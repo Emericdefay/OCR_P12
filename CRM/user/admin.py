35419cf722af777d55077eb915860aa6974979f1
emericdefay <emericdefay@gmail.com>
2021-06-08 16:30:34 +0200

    add THROUGH models edition for admin

--- CRM/user/admin.py
+++ CRM/user/admin.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 # Locals:
 from .models import (Saler,
-                     Support)
+                     SalerTHROUGH,
+                     Support,
+                     SupportTHROUGH)
 
 
 # Register user
@@ -31,9 +33,38 @@ class Role(admin.ModelAdmin):
     Allow Admins to Create/Update/Delete Saler/Support's roles.
     """
     fieldsets = [
-        ('Username', {'fields': ['user']}),
+        ('User', {'fields': ['user']}),
     ]
 
 
 admin.site.register(Saler, Role)
 admin.site.register(Support, Role)
+
+
+# Set support users to event
+class ContactSupport(admin.ModelAdmin):
+    """
+    Allow Admins to Create/Update/Delete Saler/Support's linked to event.
+    """
+    fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Client', {'fields': ['client']}),
+        ('Event', {'fields': ['event']}),
+    ]
+
+
+admin.site.register(SupportTHROUGH, ContactSupport)
+
+
+# Set sale users to client
+class ContactSaler(admin.ModelAdmin):
+    """
+    Allow Admins to Create/Update/Delete Saler/Support's linked to client.
+    """
+    fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Client', {'fields': ['client']}),
+    ]
+
+
+admin.site.register(SalerTHROUGH, ContactSaler)
