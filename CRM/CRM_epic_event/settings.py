accf7c624fdcb9f052cb0d3415a4a12396209c57
emericdefay <emericdefay@gmail.com>
2021-06-10 07:02:15 +0200

    setup for production

--- CRM/CRM_epic_event/settings.py
+++ CRM/CRM_epic_event/settings.py
@@ -84,27 +84,17 @@ WSGI_APPLICATION = 'CRM_epic_event.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'crm_database',
-#         'USER': 'django',
-#         'PASSWORD': 'Openclassroom',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
-# Database for testing
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': { 
-            'NAME': 'test.db.sqlite3',
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm_database',
+        'USER': 'django',
+        'PASSWORD': 'Openclassroom',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 LOGGING = {
     'version': 1,
